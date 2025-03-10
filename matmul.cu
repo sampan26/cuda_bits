feef69618cd3ebdafd108d223da467d7a249887a
sampan26 <rvbh87m@gmail.com>
2025-03-10 01:55:35 -0500

    matmul v5

--- matmul.cu
+++ matmul.cu
@@ -259,3 +259,118 @@ void matmul_v4(const float *A, const float *B, float *C, int M, int N, int K) {
   const int grid_size = cdiv(M * N, BLOCK_M * BLOCK_N);
   matmul_v4_kernel<BLOCK_M, BLOCK_N, BLOCK_K, THREAD_M, THREAD_N><<<grid_size, BLOCK_SIZE>>>(A, B, C, M, N, K);
 }
+
+// 2D warp-tiling with register cache
+// we partition output tile (BLOCK_M, BLOCK_N) into tiles of (WARP_M, WARP_N)
+// we use the exact number of warps in a threadblock as the number of warp tiles
+// for each output warp tile (WARP_M, WARP_N), we further divide it into MMA tiles (MMA_M, MMA_N)
+// for each MMA tile (MMA_M, MMA_N), we divide it exactly to 32 thread tiles (THREAD_M, THREAD_N),
+// since there are 32 threads in a warp.
+template <int BLOCK_M, int BLOCK_N, int BLOCK_K, int WARP_M, int WARP_N, int MMA_M, int MMA_N, int THREAD_N>
+__global__ void matmul_v5_kernel(const float *A, const float *B, float *C, int M, int N, int K) {
+    static_assert(BLOCK_M % WARP_M == 0);
+    static_assert(BLOCK_N % WARP_N == 0);
+    static_assert(WARP_M % MMA_M == 0);
+    static_assert(WARP_N % MMA_N == 0);
+    static_assert((MMA_M * MMA_N / THREAD_N) % WARP_SIZE == 0);
+
+    constexpr int BLOCK_SIZE = (BLOCK_M * BLOCK_N) / (WARP_SIZE * WARP_N) * WARP_SIZE;
+    const int NUM_MMA_M = WARP_M / MMA_M;
+    const int NUM_MMA_N = WARP_N / MMA_N;
+    const int THREAD_M = MMA_M * MMA_N / (WARP_SIZE * THREAD_N);
+
+    const int tid = threadIdx.x;
+    const int block_id = blockIdx.x;
+    const int warp_id = tid / WARP_SIZE;
+    const int lane_id = tid % WARP_SIZE;
+
+    const int num_blocks_per_row = cdiv(N, BLOCK_N);
+    const int block_id_m = block_id / num_blocks_per_row;
+    const int block_id_n = block_id % num_blocks_per_row;
+    const int offset_m = block_id_m * BLOCK_M;
+    const int offset_n = block_id_n * BLOCK_N;
+
+    const int num_warps_per_row = BLOCK_N / WARP_N;
+    const int warp_id_m = warp_id / num_warps_per_row;
+    const int warp_id_n = warp_id % num_warps_per_row;
+    const int warp_tile_offset_m = warp_id_m * WARP_M;
+    const int warp_tile_offset_n = warp_id_n * WARP_N;
+
+    const int num_threads_per_row = MMA_N / THREAD_N;
+    const int tile_thread_id_m = lane_id / num_threads_per_row;
+    const int tile_thread_id_n = lane_id % num_threads_per_row;
+    const int tile_thread_offset_m = tile_thread_id_m * THREAD_M;
+    const int tile_thread_offset_n = tile_thread_id_n * THREAD_N;
+
+    __shared__ float A_shmem[BLOCK_M][BLOCK_K];
+    __shared__ float B_shmem[BLOCK_K][BLOCK_N];
+    float acc[NUM_MMA_M][NUM_MMA_N][THREAD_M][THREAD_N] = {0.0f};
+
+    A += offset_m * K;
+    B += offset_n;
+    
+
+    const float *A_thread_tile = reinterpret_cast<const float *>(A_shmem) + (tile_thread_offset_m + warp_tile_offset_m) * BLOCK_K;
+    const float *B_thread_tile = reinterpret_cast<const float *>(B_shmem) + (tile_thread_offset_n + warp_tile_offset_n);
+
+    // points to the corresponding thread tile of the current thread in the first MMA tile
+    for (int offset_k = 0; offset_k < K; offset_k += BLOCK_K) {
+        load_shmem<BLOCK_SIZE, BLOCK_M, BLOCK_K>(A, K, M - offset_m, K - offset_k, A_shmem, tid);
+        load_shmem<BLOCK_SIZE, BLOCK_K, BLOCK_N>(B, N, K - offset_k, N - offset_n, B_shmem, tid);
+        __syncthreads();
+        
+        // implicit WARP_K = MMA_K = THREAD_K = 1
+        for (int k = 0; k < BLOCK_K; k++) {
+            float A_reg[NUM_MMA_M][THREAD_M]; // 2Dregister cache
+            float B_reg[NUM_MMA_N][THREAD_N]; 
+
+            // notice we have extra loops to iterate over MMA tiles
+            for (int mm_tile_id_m = 0; mm_tile_id_m < NUM_MMA_M; mm_tile_id_m++)
+                for (int m = 0; m < THREAD_M; m++)
+                    A_reg[mm_tile_id_m][m] = A_thread_tile[(mm_tile_id_m * MMA_M + m) * BLOCK_K + k];
+
+            for (int mm_tile_id_n = 0; mm_tile_id_n < NUM_MMA_N; mm_tile_id_n++)
+                for (int n = 0; n < THREAD_N; n++)
+                    B_reg[mm_tile_id_n][n] = B_thread_tile[k * BLOCK_N + (mm_tile_id_n * MMA_N + n)];
+
+           
+             for (int mma_tile_id_m = 0; mma_tile_id_m < NUM_MMA_M; mma_tile_id_m++)
+                for (int mma_tile_id_n = 0; mma_tile_id_n < NUM_MMA_N; mma_tile_id_n++)
+                    for (int m = 0; m < THREAD_M; m++)
+                        for (int n = 0; n < THREAD_N; n++)
+                        acc[mma_tile_id_m][mma_tile_id_n][m][n] += A_reg[mma_tile_id_m][m] * B_reg[mma_tile_id_n][n];
+        }
+        __syncthreads();
+
+        A += BLOCK_K;
+        B += BLOCK_K * N;
+    }
+
+    // points to the corresponding thread tile in the first MMA tile
+    int C_offset_m = offset_m + tile_thread_offset_m + warp_tile_offset_m;
+    int C_offset_n = offset_n + tile_thread_offset_n + warp_tile_offset_n;
+    C += C_offset_m * N + C_offset_n;
+
+    // uncoalesced memory write
+    // fixing it doesn't seem to make the kernel faster.
+    // vectorized write is slower.
+    for (int mma_m = 0; mma_m < WARP_M; mma_m += MMA_M)
+        for (int mma_n = 0; mma_n < WARP_N; mma_n += MMA_N)
+            for (int tm = 0; tm < THREAD_M; tm++)
+                for (int tn = 0; tn < THREAD_N; tn++)
+                    if ((C_offset_m + mma_m + tm < M) && (C_offset_n + mma_n + tn < N))
+                        C[(mma_m + tm) * N + (mma_n + tn)] = acc[mma_m / MMA_M][mma_n / MMA_N][tm][tn];
+}
+
+void matmul_v5(const float *A, const float *B, float *C, int M, int N, int K) {
+  // this config will result in identical kernel as v4
+
+  const int BLOCK_M = 128, BLOCK_N = 64, BLOCK_K = 64;
+  const int WARP_M = 32, WARP_N = 32;
+  const int MMA_M = 16, MMA_N = 32;
+  const int THREAD_N = 4;  // THREAD_M = MMA_M * MMA_N / 32 / THREAD_N = 4
+
+  const int BLOCK_SIZE = (BLOCK_M * BLOCK_N) / (WARP_M * WARP_N) * WARP_SIZE;  // 256
+  const int grid_size = cdiv(M * N, BLOCK_M * BLOCK_N);
+  matmul_v5_kernel<BLOCK_M, BLOCK_N, BLOCK_K, WARP_M, WARP_N, MMA_M, MMA_N, THREAD_N><<<grid_size, BLOCK_SIZE>>>(A, B, C, M, N, K);
+}
