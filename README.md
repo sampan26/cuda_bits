022d7e7cd0ddbf224fea8daaeff7522c9b6ac528
sampan26 <rvbh87m@gmail.com>
2025-03-12 12:22:33 -0500

    updated vector + edit readme to do

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # cuda_gemm
+
+# Matrix Multiplication - CUDA Cores
+
+Resources:
+- https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#shared-memory
+- https://siboehm.com/articles/22/CUDA-MMM
+- https://leimao.github.io/article/CUDA-Matrix-Multiplication-Optimization/ and https://github.com/leimao/CUDA-GEMM-Optimization/
+- https://github.com/NVIDIA/cutlass/blob/main/media/docs/efficient_gemm.md
+
+All experiments run on A6000
+Lessons learned:
+
+- Hierarchical tiling: block-level (shared memory cache), warp-level, and thread-level (register cache).
+- Vectorized memory access: not sure why it helps as we already use 128-byte memory transaction. Perhaps it reduces number of instructions and thus better pipelining?
+- Reduce memory address computations: increment memory address after each iteration instead of calculating from the base address every time.
+- Avoid bank conflicts:
+  - Padding shared memory (TODO).
+  - Swizzled layout (TODO).
+- Double buffering: use double amount of shared memory, but don't need to `__syncthreads()` after computation code -> better data loading and computation interleaving (TODO).
--- matmul.cu
+++ matmul.cu
@@ -253,11 +253,11 @@ __global__ void matmul_v4_kernel(const float *A, const float *B, float *C, int M
 }
 
 void matmul_v4(const float *A, const float *B, float *C, int M, int N, int K) {
-  const int BLOCK_M = 128, BLOCK_N = 128, BLOCK_K = 32;
-  const int THREAD_M = 8, THREAD_N = 8;
-  const int BLOCK_SIZE = (BLOCK_M * BLOCK_N) / (THREAD_M * THREAD_N);  // 256
-  const int grid_size = cdiv(M * N, BLOCK_M * BLOCK_N);
-  matmul_v4_kernel<BLOCK_M, BLOCK_N, BLOCK_K, THREAD_M, THREAD_N><<<grid_size, BLOCK_SIZE>>>(A, B, C, M, N, K);
+    const int BLOCK_M = 128, BLOCK_N = 128, BLOCK_K = 32;
+    const int THREAD_M = 8, THREAD_N = 8;
+    const int BLOCK_SIZE = (BLOCK_M * BLOCK_N) / (THREAD_M * THREAD_N);  // 256
+    const int grid_size = cdiv(M * N, BLOCK_M * BLOCK_N);
+    matmul_v4_kernel<BLOCK_M, BLOCK_N, BLOCK_K, THREAD_M, THREAD_N><<<grid_size, BLOCK_SIZE>>>(A, B, C, M, N, K);
 }
 
 // 2D warp-tiling with register cache
@@ -363,49 +363,54 @@ __global__ void matmul_v5_kernel(const float *A, const float *B, float *C, int M
 }
 
 void matmul_v5(const float *A, const float *B, float *C, int M, int N, int K) {
-  // this config will result in identical kernel as v4
+    // this config will result in identical kernel as v4
 
-  const int BLOCK_M = 128, BLOCK_N = 64, BLOCK_K = 64;
-  const int WARP_M = 32, WARP_N = 32;
-  const int MMA_M = 16, MMA_N = 32;
-  const int THREAD_N = 4;  // THREAD_M = MMA_M * MMA_N / 32 / THREAD_N = 4
+    const int BLOCK_M = 128, BLOCK_N = 64, BLOCK_K = 64;
+    const int WARP_M = 32, WARP_N = 32;
+    const int MMA_M = 16, MMA_N = 32;
+    const int THREAD_N = 4;  // THREAD_M = MMA_M * MMA_N / 32 / THREAD_N = 4
 
-  const int BLOCK_SIZE = (BLOCK_M * BLOCK_N) / (WARP_M * WARP_N) * WARP_SIZE;  // 256
-  const int grid_size = cdiv(M * N, BLOCK_M * BLOCK_N);
-  matmul_v5_kernel<BLOCK_M, BLOCK_N, BLOCK_K, WARP_M, WARP_N, MMA_M, MMA_N, THREAD_N><<<grid_size, BLOCK_SIZE>>>(A, B, C, M, N, K);
+    const int BLOCK_SIZE = (BLOCK_M * BLOCK_N) / (WARP_M * WARP_N) * WARP_SIZE;  // 256
+    const int grid_size = cdiv(M * N, BLOCK_M * BLOCK_N);
+    matmul_v5_kernel<BLOCK_M, BLOCK_N, BLOCK_K, WARP_M, WARP_N, MMA_M, MMA_N, THREAD_N><<<grid_size, BLOCK_SIZE>>>(A, B, C, M, N, K);
 }
 
+// no bounds check
+// NOTE: loop can be unrolled now since everything is known at compile time
+// this gives a small boost. for load_shmem() (non-vectorized), this is actually slower.
 template <int BLOCK_SIZE, int HEIGHT, int WIDTH, bool TRANSPOSED>
-__device__ void load_shmem_vectorized(const float*in, int in_row_stride, float *out, int tid) {
-    for (int idx = tid; idx < HEIGHT * WIDTH; idx += BLOCK_SIZE) {
-        int offset = 4 * tid + idx;
-        int row = offset / WIDTH;
-        int col = offset % WIDTH;
+__device__ void load_shmem_vectorized(const float *in, int in_row_stride, float *out, int tid) {
+  for (int offset = 0; offset < HEIGHT * WIDTH; offset += BLOCK_SIZE * 4) {
+    const int idx = offset + tid * 4;
+    const int row = idx / WIDTH;
+    const int col = idx % WIDTH;
 
-        float4 tmp = reinterpret_cast<const float4 *>(&in[row * in_row_stride + col])[0];
-        if (TRANSPOSED) {
-            out[(col + 0) * HEIGHT + row] = tmp.x;
-            out[(col + 1) * HEIGHT + row] = tmp.y;
-            out[(col + 2) * HEIGHT + row] = tmp.z;
-            out[(col + 3) * HEIGHT + row] = tmp.w;
-        } else
-            reinterpret_cast<float4 *>(&out[row * WIDTH + col])[0] = tmp;
-    }
+    float4 tmp = reinterpret_cast<const float4 *>(&in[row * in_row_stride + col])[0];
+
+    if (TRANSPOSED) {
+      out[(col + 0) * HEIGHT + row] = tmp.x;
+      out[(col + 1) * HEIGHT + row] = tmp.y;
+      out[(col + 2) * HEIGHT + row] = tmp.z;
+      out[(col + 3) * HEIGHT + row] = tmp.w;
+    } else
+      reinterpret_cast<float4 *>(&out[row * WIDTH + col])[0] = tmp;
+  }
 }
 
+// vectorized memory access without bounds check
+// only memory access is different from v5
 template <int BLOCK_M, int BLOCK_N, int BLOCK_K, int WARP_M, int WARP_N, int MMA_M, int MMA_N, int THREAD_N, bool TRANSPOSE_A_shmem>
 __global__ void matmul_v6_kernel(const float *A, const float *B, float *C, int M, int N, int K) {
     static_assert(BLOCK_M % WARP_M == 0);
     static_assert(BLOCK_N % WARP_N == 0);
     static_assert(WARP_M % MMA_M == 0);
     static_assert(WARP_N % MMA_N == 0);
-    static_assert(THREAD_N % 4 == 0);  // so we can use vectorized access
     static_assert((MMA_M * MMA_N / THREAD_N) % WARP_SIZE == 0);
-
-    constexpr int BLOCK_SIZE = (BLOCK_M * BLOCK_N) / (WARP_SIZE * WARP_N) * WARP_SIZE;
-    const int NUM_MMA_M = WARP_M / MMA_M;
-    const int NUM_MMA_N = WARP_N / MMA_N;
-    const int THREAD_M = MMA_M * MMA_N / (WARP_SIZE * THREAD_N);
+    static_assert(THREAD_N % 4 == 0);  // so we can use vectorized access
+    constexpr int BLOCK_SIZE = (BLOCK_M * BLOCK_N) / (WARP_M * WARP_N) * WARP_SIZE;
+    constexpr int NUM_MMA_M = WARP_M / MMA_M;
+    constexpr int NUM_MMA_N = WARP_N / MMA_N;
+    constexpr int THREAD_M = MMA_M * MMA_N / (WARP_SIZE * THREAD_N);
 
     const int tid = threadIdx.x;
     const int block_id = blockIdx.x;
@@ -418,99 +423,107 @@ __global__ void matmul_v6_kernel(const float *A, const float *B, float *C, int M
     const int offset_m = block_id_m * BLOCK_M;
     const int offset_n = block_id_n * BLOCK_N;
 
-    const int num_warps_per_row = BLOCK_N / WARP_N;
+    constexpr int num_warps_per_row = BLOCK_N / WARP_N;
     const int warp_id_m = warp_id / num_warps_per_row;
     const int warp_id_n = warp_id % num_warps_per_row;
     const int warp_tile_offset_m = warp_id_m * WARP_M;
     const int warp_tile_offset_n = warp_id_n * WARP_N;
 
-    const int num_threads_per_row = MMA_N / THREAD_N;
-    const int tile_thread_id_m = lane_id / num_threads_per_row;
-    const int tile_thread_id_n = lane_id % num_threads_per_row;
-    const int tile_thread_offset_m = tile_thread_id_m * THREAD_M;
-    const int tile_thread_offset_n = tile_thread_id_n * THREAD_N;
+    constexpr int num_thread_tiles_per_row = MMA_N / THREAD_N;
+    const int thread_tile_id_m = lane_id / num_thread_tiles_per_row;
+    const int thread_tile_id_n = lane_id % num_thread_tiles_per_row;
+    const int thread_tile_offset_m = thread_tile_id_m * THREAD_M;
+    const int thread_tile_offset_n = thread_tile_id_n * THREAD_N;
+
+    A += offset_m * K;
+    B += offset_n;
 
     __shared__ float A_shmem[BLOCK_M * BLOCK_K];
     __shared__ float B_shmem[BLOCK_K * BLOCK_N];
     float acc[NUM_MMA_M][NUM_MMA_N][THREAD_M][THREAD_N] = {0.0f};
 
-    A += offset_m * K;
-    B += offset_n;
-    
+    const float *A_thread_tile = reinterpret_cast<const float *>(A_shmem) + (warp_tile_offset_m + thread_tile_offset_m) * (TRANSPOSE_A_shmem ? 1 : BLOCK_K);
+    const float *B_thread_tile = reinterpret_cast<const float *>(B_shmem) + (warp_tile_offset_n + thread_tile_offset_n);
 
-    const float *A_thread_tile = reinterpret_cast<const float *>(A_shmem) + (tile_thread_offset_m + warp_tile_offset_m) * (TRANSPOSE_A_shmem ? 1 : BLOCK_K);
-    const float *B_thread_tile = reinterpret_cast<const float *>(B_shmem) + (tile_thread_offset_n + warp_tile_offset_n);
-
-    // points to the corresponding thread tile of the current thread in the first MMA tile
     for (int offset_k = 0; offset_k < K; offset_k += BLOCK_K) {
         load_shmem_vectorized<BLOCK_SIZE, BLOCK_M, BLOCK_K, TRANSPOSE_A_shmem>(A, K, A_shmem, tid);
         load_shmem_vectorized<BLOCK_SIZE, BLOCK_K, BLOCK_N, false>(B, N, B_shmem, tid);
         __syncthreads();
-        
-        // implicit WARP_K = MMA_K = THREAD_K = 1
+
         for (int k = 0; k < BLOCK_K; k++) {
-            float A_reg[NUM_MMA_M][THREAD_M];
-            float B_reg[NUM_MMA_N][THREAD_N];
+        float A_reg[NUM_MMA_M][THREAD_M];
+        float B_reg[NUM_MMA_N][THREAD_N];
 
-            for (int mma_tile_id_m = 0; mma_tile_id_m < NUM_MMA_M; mma_tile_id_m++)
-                if (TRANSPOSE_A_shmem) {
-                static_assert(THREAD_M % 4 == 0);
-                for (int tm = 0; tm < THREAD_M; tm += 4) {
-                    float4 tmp = reinterpret_cast<const float4 *>(&A_thread_tile[k * BLOCK_M + (mma_tile_id_m * MMA_M + tm)])[0];
-                    reinterpret_cast<float4 *>(&A_reg[mma_tile_id_m][tm])[0] = tmp;
-                }
-                }
-                else {
-                for (int tm = 0; tm < THREAD_M; tm++)
-                    A_reg[mma_tile_id_m][tm] = A_thread_tile[(mma_tile_id_m * MMA_M + tm) * BLOCK_K + k];
-                }
-
-            for (int mma_tile_id_n = 0; mma_tile_id_n < NUM_MMA_N; mma_tile_id_n++)
-                for (int tn = 0; tn < THREAD_N; tn += 4) {
-                float4 tmp = reinterpret_cast<const float4 *>(&B_thread_tile[k * BLOCK_N + (mma_tile_id_n * MMA_N + tn)])[0];
-                reinterpret_cast<float4 *>(&B_reg[mma_tile_id_n][tn])[0] = tmp;
-                }
-
-            for (int mma_tile_id_m = 0; mma_tile_id_m < NUM_MMA_M; mma_tile_id_m++)
-                for (int mma_tile_id_n = 0; mma_tile_id_n < NUM_MMA_N; mma_tile_id_n++)
-                for (int tm = 0; tm < THREAD_M; tm++)
-                    for (int tn = 0; tn < THREAD_N; tn++)
-                    acc[mma_tile_id_m][mma_tile_id_n][tm][tn] += A_reg[mma_tile_id_m][tm] * B_reg[mma_tile_id_n][tn];
+        for (int mma_tile_id_m = 0; mma_tile_id_m < NUM_MMA_M; mma_tile_id_m++)
+            if (TRANSPOSE_A_shmem) {
+            static_assert(THREAD_M % 4 == 0);
+            for (int tm = 0; tm < THREAD_M; tm += 4) {
+                float4 tmp = reinterpret_cast<const float4 *>(&A_thread_tile[k * BLOCK_M + (mma_tile_id_m * MMA_M + tm)])[0];
+                reinterpret_cast<float4 *>(&A_reg[mma_tile_id_m][tm])[0] = tmp;
             }
+            }
+            else {
+            for (int tm = 0; tm < THREAD_M; tm++)
+                A_reg[mma_tile_id_m][tm] = A_thread_tile[(mma_tile_id_m * MMA_M + tm) * BLOCK_K + k];
+            }
+
+        for (int mma_tile_id_n = 0; mma_tile_id_n < NUM_MMA_N; mma_tile_id_n++)
+            for (int tn = 0; tn < THREAD_N; tn += 4) {
+            float4 tmp = reinterpret_cast<const float4 *>(&B_thread_tile[k * BLOCK_N + (mma_tile_id_n * MMA_N + tn)])[0];
+            reinterpret_cast<float4 *>(&B_reg[mma_tile_id_n][tn])[0] = tmp;
+            }
+
+        for (int mma_tile_id_m = 0; mma_tile_id_m < NUM_MMA_M; mma_tile_id_m++)
+            for (int mma_tile_id_n = 0; mma_tile_id_n < NUM_MMA_N; mma_tile_id_n++)
+            for (int tm = 0; tm < THREAD_M; tm++)
+                for (int tn = 0; tn < THREAD_N; tn++)
+                acc[mma_tile_id_m][mma_tile_id_n][tm][tn] += A_reg[mma_tile_id_m][tm] * B_reg[mma_tile_id_n][tn];
+        }
         __syncthreads();
 
         A += BLOCK_K;
         B += BLOCK_K * N;
-    }
+  }
 
-    // points to the corresponding thread tile in the first MMA tile
-    int C_offset_m = offset_m + tile_thread_offset_m + warp_tile_offset_m;
-    int C_offset_n = offset_n + tile_thread_offset_n + warp_tile_offset_n;
+    const int C_offset_m = offset_m + warp_tile_offset_m + thread_tile_offset_m;
+    const int C_offset_n = offset_n + warp_tile_offset_n + thread_tile_offset_n;
     C += C_offset_m * N + C_offset_n;
 
-    // uncoalesced memory write
-    // fixing it doesn't seem to make the kernel faster.
-    // vectorized write is slower.
     for (int mma_m = 0; mma_m < WARP_M; mma_m += MMA_M)
         for (int mma_n = 0; mma_n < WARP_N; mma_n += MMA_N)
             for (int tm = 0; tm < THREAD_M; tm++)
-                for (int tn = 0; tn < THREAD_N; tn++) {
-                        const float4 tmp = reinterpret_cast<const float4 *>(&acc[mma_m / MMA_M][mma_n / MMA_N])[0];
-                        reinterpret_cast<float4 *>(&C[(mma_m + tm) * N + (mma_n + tn)])[0] = tmp;
+                for (int tn = 0; tn < THREAD_N; tn += 4) {
+                    const float4 tmp = reinterpret_cast<const float4 *>(&acc[mma_m / MMA_M][mma_n / MMA_N][tm][tn])[0];
+                    reinterpret_cast<float4 *>(&C[(mma_m + tm) * N + (mma_n + tn)])[0] = tmp;
                 }
 }
 
-void matmul_v6(const float *A, const float *B, float *C, int M, int N, int K) {
-  assert(is_power_of_two(M) && "M must be a power of 2");
-  assert(is_power_of_two(N) && "N must be a power of 2");
-  assert(is_power_of_two(K) && "K must be a power of 2");
+void matmul_v6a(const float *A, const float *B, float *C, int M, int N, int K) {
+    assert(is_power_of_two(M) && "M must be a power of 2");
+    assert(is_power_of_two(N) && "N must be a power of 2");
+    assert(is_power_of_two(K) && "K must be a power of 2");
 
-  const int BLOCK_M = 128, BLOCK_N = 128, BLOCK_K = 16;
-  const int WARP_M = 64, WARP_N = 64;
-  const int MMA_M = 16, MMA_N = 32;
-  const int THREAD_N = 4;  // THREAD_M = MMA_M * MMA_N / 32 / THREAD_N = 4
+    const int BLOCK_M = 128, BLOCK_N = 128, BLOCK_K = 16;
+    const int WARP_M = 64, WARP_N = 64;
+    const int MMA_M = 16, MMA_N = 32;
+    const int THREAD_N = 4;  // THREAD_M = MMA_M * MMA_N / 32 / THREAD_N = 4
 
-  const int BLOCK_SIZE = (BLOCK_M * BLOCK_N) / (WARP_M * WARP_N) * WARP_SIZE;  // 128
-  const int grid_size = cdiv(M * N, BLOCK_M * BLOCK_N);
-  matmul_v6_kernel<BLOCK_M, BLOCK_N, BLOCK_K, WARP_M, WARP_N, MMA_M, MMA_N, THREAD_N, true><<<grid_size, BLOCK_SIZE>>>(A, B, C, M, N, K);
+    const int BLOCK_SIZE = (BLOCK_M * BLOCK_N) / (WARP_M * WARP_N) * WARP_SIZE;  // 128
+    const int grid_size = cdiv(M * N, BLOCK_M * BLOCK_N);
+    matmul_v6_kernel<BLOCK_M, BLOCK_N, BLOCK_K, WARP_M, WARP_N, MMA_M, MMA_N, THREAD_N, false><<<grid_size, BLOCK_SIZE>>>(A, B, C, M, N, K);
+}
+
+void matmul_v6b(const float *A, const float *B, float *C, int M, int N, int K) {
+    assert(is_power_of_two(M) && "M must be a power of 2");
+    assert(is_power_of_two(N) && "N must be a power of 2");
+    assert(is_power_of_two(K) && "K must be a power of 2");
+
+    const int BLOCK_M = 64, BLOCK_N = 128, BLOCK_K = 8;
+    const int WARP_M = 64, WARP_N = 64;
+    const int MMA_M = 32, MMA_N = 32;
+    const int THREAD_N = 4;  // THREAD_M = MMA_M * MMA_N / 32 / THREAD_N = 8
+
+    const int BLOCK_SIZE = (BLOCK_M * BLOCK_N) / (WARP_M * WARP_N) * WARP_SIZE;  // 64
+    const int grid_size = cdiv(M * N, BLOCK_M * BLOCK_N);
+    matmul_v6_kernel<BLOCK_M, BLOCK_N, BLOCK_K, WARP_M, WARP_N, MMA_M, MMA_N, THREAD_N, true><<<grid_size, BLOCK_SIZE>>>(A, B, C, M, N, K);
 }
