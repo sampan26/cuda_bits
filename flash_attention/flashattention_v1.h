c47d6e2194ad0b3d02e4b6ba8ebf2201ee90a3c5
sampan26 <rvbh87m@gmail.com>
2025-04-13 00:22:28 -0500

    bro

--- flash_attention/flashattention_v1.h
+++
@@ -1,7 +0,0 @@
-#pragma once // Include guard
-#include <stdio.h>
-
-// Declare the LAUNCHER function (callable from host code)
-void flashattn_v1(const float *Q, const float *K, const float *V, float *O,
-                  float *l, float *m, // Pass pointers to l and m buffers
-                  int B, int nh, int T, int head_dim);
