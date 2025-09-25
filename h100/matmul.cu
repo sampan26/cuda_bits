23048985f54850c821bb76cbd280e87b1fbb62a5
fastidiant <wackboi87@gmail.com>
2025-09-25 00:22:52 -0700

    c refactor

---
+++ h100/matmul.cu
@@ -0,0 +1,132 @@
+#include <cuda.h>
+#include <cudaTypedefs.h>
+#include <cuda/barrier>
+#include <cublas_v2.h>
+#include <cuda_runtime.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <ctime>
+#include <iostream>
+#include <vector>
+#include <random>
+#include <cuda_bf16.h>
+#include <cassert>
+#include <unistd.h>
+
+typedef __nv_bfloat16 bf16;
+#define CEIL_DIV(M, N) (((M) + (N)-1) / (N))
+
+#include "src/matmul_v1.cuh"
+
+std::default_random_engine generator(69);
+
+cublasHandle_t cublas_handle;
+void runCublasGemmBF16(int M, int N, int K, bf16 *A, bf16 *B, bf16 *C) {
+    float alpha = 1; beta = 0;
+    cublasStatus_t status = cublasGemmEx(cublas_handle, CUBLAS_OP_T, CUBLAS_OP_N, M, N, K, &alpha, A, CUDA_R_16BF,
+                                        N, B, CUDA_R_16BF, K, &beta, CUDA_R_16BF, N, CUBLAS_COMPUTE_32F, CUBLAS_GEMM_DEFAULT);
+    if (status != CUBLAS_STATUS_SUCCESS) {
+        std::cout << "CUBLAS error: " << status << std::endl;
+        exit(1);
+    }
+}
+void run_kernel(int kernel_num, int M, int N, int K, bf16 *A, bf16 *B, bf16 *C, int *DB = nullptr) {
+    switch (kernel_num) {
+        case 0:
+        runCublasGemmBF16(M, N, K, A, B, C);
+        break;
+        case 1:
+        runKernel1(M, N, K, A, B, C);
+        break;
+    }
+}
+    
+int yo = 0;
+void randomize_matrix(bf16 *mat, int N) {
+  std::normal_distribution<float> distribution(0, 1);
+  for (int i = 0; i < N; i++) {
+    mat[i] = distribution(generator);
+  }
+  ++yo;
+}
+
+bool verify_matrix(bf16 *matRef, bf16 *matOut, int N) {
+    double diff = 0.0;
+    int i;
+    for (i = 0; i < N; i++) {
+      int r = i / 8192, c = i % 8192;
+      int it = c*8192+r;
+      diff = std::fabs(__bfloat162float(matRef[i] - matOut[i]));
+      if (diff > 0.1) {
+        printf("Divergence! Should %5.2f, Is %5.2f (Diff %5.2f) at %d\n",
+        __bfloat162float(matRef[i]), __bfloat162float(matOut[i]), diff, i);
+        return false;
+      }
+    }
+    return true;
+}
+
+
+int main() {
+    cublasCreate(&cublas_handle);
+
+    long max_size = 8192;
+    long m = max_size, n = max_size, k = max_size;
+
+    bf16 *A = nullptr, *B = nullptr, *C = nullptr,
+        *C_ref = nullptr;  // host matrices
+    bf16 *dA = nullptr, *dB = nullptr, *dC = nullptr,
+            *dC_ref = nullptr; // device matrices
+    
+    int *DB = nullptr; int *dDB = nullptr;  
+
+    A = (bf16 *)malloc(sizeof(bf16) * max_size * max_size);
+    B = (bf16 *)malloc(sizeof(bf16) * max_size * max_size);
+    C = (bf16 *)malloc(sizeof(bf16) * max_size * max_size);
+    C_ref = (bf16 *)malloc(sizeof(bf16) * max_size * max_size);
+    DB = (int *)malloc(sizeof(int) * max_size * 128);
+    cudaCheck(cudaMalloc((void **)&dDB, sizeof(int) * max_size * 128));
+
+    randomize_matrix(A, max_size * max_size);
+    randomize_matrix(B, max_size * max_size);
+    randomize_matrix(C, max_size * max_size);
+
+    cudaCheck(cudaMalloc((void **)&dA, sizeof(bf16) * max_size * max_size));
+    cudaCheck(cudaMalloc((void **)&dB, sizeof(bf16) * max_size * max_size));
+    cudaCheck(cudaMalloc((void **)&dC, sizeof(bf16) * max_size * max_size));
+    cudaCheck(cudaMalloc((void **)&dC_ref, sizeof(bf16) * max_size * max_size));
+    
+    cudaCheck(cudaMemcpy(dA, A, sizeof(bf16) * max_size * max_size,
+    cudaMemcpyHostToDevice));
+    cudaCheck(cudaMemcpy(dB, B, sizeof(bf16) * max_size * max_size,
+        cudaMemcpyHostToDevice));
+
+    for (int kernel_num : {0, 1}) {
+        // for (int kernel_num : {0, 11}) {
+        // Give the GPU some rest to avoid thermal throttling
+        sleep(5);
+        std::cout << "KERNEL " << kernel_num << std::endl;
+
+        memset(C, 0, sizeof(bf16) * max_size * max_size);
+        cudaCheck(cudaMemcpy(dC, C, sizeof(bf16) * max_size * max_size, cudaMemcpyHostToDevice));
+        cudaCheck(cudaMemcpy(dC_ref, C, sizeof(bf16) * max_size * max_size, cudaMemcpyHostToDevice));
+        memset(DB, ~0, sizeof(int) * max_size * 128);
+        cudaCheck(cudaMemcpy(dDB, DB, sizeof(int) * max_size * 128,
+          cudaMemcpyHostToDevice));
+        run_kernel(0, m, n, k, dA, dB, dC_ref); // cuBLAS
+        run_kernel(kernel_num, m, n, k, dA, dB, dC, dDB); // Executes the kernel, modifies the result matrix
+        cudaCheck(cudaDeviceSynchronize());
+        cudaCheck(cudaGetLastError()); // Check for async errors during kernel run
+        cudaMemcpy(C, dC, sizeof(bf16) * max_size * max_size, cudaMemcpyDeviceToHost);
+        cudaMemcpy(C_ref, dC_ref, sizeof(bf16) * max_size * max_size, cudaMemcpyDeviceToHost);
+
+        if (kernel_num > 1 && !verify_matrix(C_ref, C, m * n)) {
+          std::cout << "~~~~~~~~~~~~~~~~ Failed to pass the correctness verification against cuBLAS. ~~~~~~~~~~~~~~~~" << std::endl;
+          pri
+          
+          cudaMemcpy(DB, dDB, sizeof(int) * max_size * 8, cudaMemcpyDeviceToHost);ntf("%f\n", __bfloat162float(C_ref[m]));
+        }
+    }
+}
---
+++ h100/src/matmul_v1.cu
@@ -0,0 +1,147 @@
+namespace M2 {
+
+using barrier = cuda::barrier<cuda::thread_scope_block>;
+using cde = cuda::device::experimental;
+
+__device__ static inline uint64_t matrix_descriptor_encode(uint64_t x) { return (((x) & 0x3FFFF) >> 0x4); }
+
+__device__ uint64_t make_smem_desc(bf16* ptr) {
+  uint32_t addr = static_cast<uint32_t>(__cvta_generic_to_shared(ptr));
+  uint64_t desc = 0x0000000000000000;
+  desc |= matrix_descriptor_encode(addr);
+  desc |= matrix_descriptor_encode((uint64_t)16) << 16;
+  desc |= matrix_descriptor_encode((uint64_t)1024) << 32;
+  desc |= 1llu << 62; // 128B swizzle
+  return desc;
+}
+
+
+__device__ void warpgroup_arrive() {
+  asm volatile("wgmma.fence.sync.aligned;\n" ::: "memory");
+}
+
+__device__ void warpgroup_commit_batch() {
+  asm volatile("wgmma.commit_group.sync.aligned;\n" ::: "memory");
+}
+
+template <int N>
+__device__ void warpgroup_wait() {
+  asm volatile("wgmma.wait_group.sync.aligned %0;\n" ::"n"(N) : "memory");
+}
+
+template<int ScaleD, int ScaleA, int ScaleB, int TransA, int TransB>
+__device__ void wgmma_m64n64k16(float d[4][8], bf16 *sA, bf16 *sB) {
+  uint64_t desc_a = make_smem_desc(&sA[0]);
+  uint64_t desc_b = make_smem_desc(&sB[0]);
+
+  asm volatile(
+    "{\n"
+    "wgmma.mma_async.sync.aligned.m64n64k16.f32.bf16.bf16 "
+    "{%0,   %1,   %2,   %3,   %4,   %5,   %6,   %7,   "
+    " %8,   %9,   %10,  %11,  %12,  %13,  %14,  %15,  "
+    " %16,  %17,  %18,  %19,  %20,  %21,  %22,  %23,  "
+    " %24,  %25,  %26,  %27,  %28,  %29,  %30,  %31},"
+    " %32,"
+    " %33,"
+    " %34, %35, %36, %37, %38;\n"
+    "}\n"
+    : "+f"(d[0][0]), "+f"(d[0][1]), "+f"(d[0][2]), "+f"(d[0][3]), "+f"(d[0][4]), "+f"(d[0][5]),
+      "+f"(d[0][6]), "+f"(d[0][7]), "+f"(d[1][0]), "+f"(d[1][1]), "+f"(d[1][2]), "+f"(d[1][3]),
+      "+f"(d[1][4]), "+f"(d[1][5]), "+f"(d[1][6]), "+f"(d[1][7]), "+f"(d[2][0]), "+f"(d[2][1]),
+      "+f"(d[2][2]), "+f"(d[2][3]), "+f"(d[2][4]), "+f"(d[2][5]), "+f"(d[2][6]), "+f"(d[2][7]),
+      "+f"(d[3][0]), "+f"(d[3][1]), "+f"(d[3][2]), "+f"(d[3][3]), "+f"(d[3][4]), "+f"(d[3][5]),
+      "+f"(d[3][6]), "+f"(d[3][7])
+    : "l"(desc_a), "l"(desc_b), "n"(int32_t(ScaleD)), "n"(int32_t(ScaleA)),
+      "n"(int32_t(ScaleB)), "n"(int32_t(TransA)), "n"(int32_t(TransB)));
+}
+
+template <int BlockMajorSize, int BlockMinorSize>
+void create_tensor_map(CUtensorMap* tma_map, bf16 *src, int shape_major, int shape_minor) {
+  void *src_addr = (void*)src;
+  uint64_t gmem_prob_shape[5] = {shape_major, shape_major, 1, 1, 1};
+  uint64_t gmem_prob_stride[5] = {sizeof(bf16), sizeof(bf16) * shape_minor,0,0,0};
+  uint32_t smem_box_shape[5] = {uint32_t(BlockMinorSize), uint32_t(BlockMajorSize), 1, 1, 1};
+  uint32_t smem_box_stride[5] = {1,1,1,1,1}; 
+
+  CUresult result = cuTensorMapEncodeTiled( tma_map, CU_TENSOR_MAP_DATA_TYPE_BFLOAT16, 
+                    2, gmem_address, gmem_prob_shape, gmem_prob_stride + 1, smem_box_shape,
+                    smem_box_stride, CU_TENSOR_MAP_INTERLEAVE_NONE, CU_TENSOR_MAP_SWIZZLE_128B, 
+                    CU_TENSOR_MAP_L2_PROMOTION_NONE, CU_TENSOR_MAP_FLOAT_OOB_FILL_NONE);
+}
+
+CUtensorMap *d_tma_map_A = 0;
+CUtensorMap *d_tma_map_B = 0;
+
+__host__ static CUtensorMap* init_tensor_map(bf16* src, int shape_major, int shape_minor) {
+  CUtensorMap *tma_map_d;
+  cudaMalloc(&tma_map_d, sizeof(CUtensorMap));
+  CUtensorMap *tma_map_h;
+  create_tensor_map(&tma_map_h, src, height, width);
+  cudaMemcpy(tma_map_d, &tma_map_d, sizeof(CUtensorMap), cudaMemcpyHostToDevice);
+  return tma_map_d;
+}
+
+template<int BM, int BN, int BK, int WGMMA_M, int WGMMA_N, int WGMMA_K, int NUM_THREADS>
+__global__ void __launch_bounds__(NUM_THREADS) 
+matmul_kernel_v1(int M, int N, int K, bf16* C, 
+                 const CUtensorMap* tensorMapA, 
+                 const CUtensorMap* tensorMapB) {
+    __shared__ alignas(128) bf16 sA[BM * BK];
+    __shared__ alignas(128) bf16 sB[BN * BK];
+    float d[WGMMA_N/16][8];
+    memset(d, 0, sizeof(d));
+
+    const int num_tiles_k = K / BK;
+    const int num_rows_n = N / BN;
+    const int tile_m = blockIdx.x / num_rows_n;
+    const int tile_n = blockIdx.y % num_rows_n;
+
+    __shared__ barrier bar_A;
+    __shared__ barrier bar_B;
+
+    if (threadIdx.x == 0) {
+      init(&bar_A, blockDim.x);
+      init(&bar_B, blockDim.x);
+      cde::fence_proxy_async_shared_cta();
+    }
+    __syncthreads();
+
+    barrier::arrival_token token_A, token_B;
+
+    for (int k_tile = 0; k_tile < num_tiles_k; ++k) {
+      if (threadIdx.x == 0) {
+        cde::cp_async_bulk_tensor_2d_global_to_shared(&sA[0], tensorMapA, k_tile*BK, tile_m*BM, bar_A);
+        token_A = cuda::device::barrier_arrive_tx(barA, 1, sizeof(sA));
+        cde::cp_async_bulk_tensor_2d_global_to_shared(&sB[0], tensorMapB, k_tile*BK, tile_n*BN, bar_B);
+        token_B = cuda::device::barrier_arrive_tx(barB, 1, sizeof(sB));
+      }
+      barA.wait(std::move(token_A));
+      barA.wait(std::move(token_B));
+      __syncthreads();
+
+
+      warpgroup_arrive();
+      wgmma_m64n64k16<1, 1, 1, 0, 0>(&sA[0], &sB[0]);
+      wgmma_m64n64k16<1, 1, 1, 0, 0>(&sA[0], &sB[0]);
+      wgmma_m64n64k16<1, 1, 1, 0, 0>(&sA[0], &sB[0]);
+      wgmma_m64n64k16<1, 1, 1, 0, 0>(&sA[0], &sB[0]);
+      warpgroup_commit_batch();
+      warpgroup_wait<0>();
+    }
+
+}
+void matmul_v1(int M, int N, int K, bf16 *A, bf16 *B, bf16 *C) {
+  constexpr int BM = 64;
+  constexpr int BN = 64;
+  constexpr int BK = 64;
+  constexpr int NUM_THREADS = 128;
+  
+  tma_map_A = init_tensor_map(A, M, K);
+  tma_map_B = init_tensor_map(B, N, K);
+
+  matmul_kernel_v1<BM,BN,BK, 64, 64, 16, NUM_THREADS><<<(M/BM) * (N/BN), NUM_THREADS>>>(
+    M, N, K, C, d_tma_map_A, d_tma_map_B);
+}
+
+}
+using M2:matmul_v1
