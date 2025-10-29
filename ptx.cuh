1b784ebb88bfbbcbef390f1b077363e033d20cd9
sampan26 <114320314+sampan26@users.noreply.github.com>
2025-10-29 01:00:21 -0700

    Create ptx.cuh

---
+++ ptx.cuh
@@ -0,0 +1,64 @@
+#pragma once
+#include <cuda.h>
+#include <cuda_fp16.h>
+#include <cuda_bf16.h>
+#include <cuda/pipeline>  
+
+__device__ static __forceinline__ void init_barriers(uint64_t* bar, int count) {
+    uint32_t bar_addr = static_cast<uint32_t>(__cvta_generic_to_shared(bar));
+    asm volatile("mbarrier.init.shared::cta.b64 [%0], %1;\n"
+        :: "r"(bar_addr), "r"(count)
+    );
+}
+
+__device__ static __forceinline__ void expect_bytes(uint64_t* bar, uint32_t bytes) {
+    uint32_t bar_addr = static_cast<uint32_t>(__cvta_generic_to_shared(bar));
+    asm volatile("mbarrier.arrive.expect_tx.release.cta.shared::cta.b64 _, [%0], %1;\n"
+        :: "r"(bar_addr), "r"(expect_bytes)
+    );
+}
+
+__device__ static __forceinline__ void wait(uint64_t* bar, int kPhaseBit) {
+    uint32_t mbar_ptr = static_cast<uint32_t>(__cvta_generic_to_shared(bar)); 
+    asm volatile (
+        "{\n"
+        ".reg .pred                P1;\n"
+        "LAB_WAIT:\n"
+        "mbarrier.try_wait.parity.acquire.cta.shared::cta.b64 P1, [%0], %1;\n"
+        "@P1                       bra.uni DONE;\n"
+        "bra.uni                   LAB_WAIT;\n"
+        "DONE:\n"
+        "}\n"
+        :: "r"(mbar_ptr),
+        "r"(kPhaseBit)
+    );
+}
+
+// tma helpers
+template <int SWIZZLE_ELEMENTS>
+__device__ static inline void tma_load(nv_bfloat16 *dst, void const* const src_tma_map, uint64_t* bar, int global_col_idx, int global_row_idx) {
+    uint64_t tma_ptr  = reinterpret_cast<uint64_t>(src_tma_map);
+    uint32_t mbar_ptr = static_cast<uint32_t>(__cvta_generic_to_shared(bar));
+    uint32_t dst_ptr  = static_cast<uint32_t>(__cvta_generic_to_shared(dst));
+
+    asm volatile (
+        "cp.async.bulk.tensor.5d.shared::cta.global.tile.mbarrier::complete_tx::bytes.cta_group::1 "
+        " [%0], [%1, {%3, %4, %5, 0, 0}], [%2];"
+        :
+        : "r"(dst_ptr), "l"(tma_ptr), "r"(mbar_ptr),
+        "n"(0), "r"(global_row_idx), "r"(global_col_idx/SWIZZLE_ELEMENTS)
+        : "memory"
+    );
+}
+
+
+// tcgen05 helpers
+__device__ static __forceinline__ void tmem_alloc(uint64_t& dst_addr, int n_cols) {
+    asm volatile("tcgen05.alloc.cta_group::1.sync.aligned.b32 [%0], %1;\n"
+        :: "l"((uint64_t)&dst_addr), "r"(n_cols)
+    );
+}
+
+__device__ __forceinline__ void tcgen05_mma(nv_bfloat16* sA, nv_bfloat16* sB) {
+    
+}
